63f614173c7ec4665f005fc3bc7e1799b485e651
ishwaryanam <inamburu@calpoly.edu>
2023-10-06 15:47:21 -0700

    feat: testing styles

--- styles.cc
+++ styles.cc
@@ -1,1 +1,30 @@
-gi
+body {
+  font-size: 14px;
+}
+
+p {
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 14px;
+}
+
+.class {
+  color: red;
+  background-color: blue;
+}
+
+#id {
+  color: green;
+  background-color: yellow;
+}
+
+p.title {
+  font-style: italic;
+  font-weight: 500;
+  font-size: 16px;
+}
+
+h1#main-title {
+  color: red;
+  background-color: blue;
+}
