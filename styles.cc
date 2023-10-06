049337bc447d187cf1ac6099c5c3c1c7ee455a2b
ishwaryanam <inamburu@calpoly.edu>
2023-10-06 15:53:05 -0700

    feat: testing styles

--- styles.cc
+++ styles.cc
@@ -1,30 +1,8 @@
+:root{
+    /*variables*/
+    --cg-grey: rgb(243,243,243);
+}
 body {
+  background: var(--cg-grey);
   font-size: 14px;
-}
-
-p {
-  font-family: sans-serif;
-  font-weight: bold;
-  font-size: 14px;
-}
-
-.class {
-  color: red;
-  background-color: blue;
-}
-
-#id {
-  color: green;
-  background-color: yellow;
-}
-
-p.title {
-  font-style: italic;
-  font-weight: 500;
-  font-size: 16px;
-}
-
-h1#main-title {
-  color: red;
-  background-color: blue;
-}
+};
