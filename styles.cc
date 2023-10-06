7fe267e149d3ae6900327fabcca22de3bef55ea4
ishwaryanam <inamburu@calpoly.edu>
2023-10-06 15:59:19 -0700

    feat: testing

--- styles.cc
+++ styles.cc
@@ -7,3 +7,25 @@ body {
   font-size: 14px;
 }
 
+.navbar {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 7%;
+  z-index: 10;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+
+  background-color: var(--cg-black);
+  color: var(--cg-white);
+  padding: 0px 0px;
+
+  /* Debug border */
+  /* border: solid red; */
+}
+
+
